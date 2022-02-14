99859a213fc75d31e1917cf60043ddde638c011f
Mike Loerop <maloerop@gmail.com>
2022-02-13 21:43:44 -0500

    Initial commit

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,26 @@
+﻿using AddressBookNet6.Services.Interfaces;
+
+namespace AddressBookNet6.Services
+{
+    public class BasicImageService : IImageService
+    {
+
+        public string CovertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData is null) return string.Empty;
+
+            string imageBase64data = Convert.ToBase64String(fileData);
+            return $"data:{extension};base64,{imageBase64data}";
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            using MemoryStream memoryStream = new();
+            await file.CopyToAsync(memoryStream);
+            byte[] byteFile = memoryStream.ToArray();
+
+            return byteFile;
+
+        }
+    }
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace AddressBookNet6.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string CovertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
